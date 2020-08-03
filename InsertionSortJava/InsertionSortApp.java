10477d5a8c27f16f199221a219f27e5774ed1f9d
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-03 20:29:37 +0530

    insertionSortJava

---
+++ InsertionSortJava/InsertionSortApp.java
@@ -0,0 +1,29 @@
+package InsertionSortJava;
+
+public class InsertionSortApp {
+
+    public static void main(String[] args) {
+        int maxSize = 100;
+        InsertionSort arr;
+        arr = new InsertionSort(maxSize);
+        // for (int i = 0; i < 100; i++) {
+        // arr.insert((int) Math.random() * 100);
+        // }
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        arr.display();
+
+        arr.insertionSort();
+        arr.display();
+
+    }
+
+}
