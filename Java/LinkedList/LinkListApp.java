f5043b4a52a3ef757c7175ea3f4013f2e2197cc3
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-28 17:29:34 +0530

    LinkedList

---
+++ Java/LinkedList/LinkListApp.java
@@ -0,0 +1,25 @@
+package Java.LinkedList;
+
+public class LinkListApp {
+    public static void main(String[] args) {
+        LinkList myList = new LinkList();
+
+        myList.insertFirst(22, 2.99);
+        myList.insertFirst(44, 4.99);
+
+        myList.insertFirst(66, 6.99);
+        myList.insertFirst(88, 8.99);
+
+        myList.displayList();
+
+        while (!myList.isEmpty()) {
+            Link aLink = myList.deleteFirst();
+            System.out.println("Deleted");
+            aLink.displayLink();
+            System.out.println("");
+            myList.displayList();
+
+        }
+        myList.displayList();
+    }
+}
